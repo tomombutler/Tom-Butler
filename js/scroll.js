e3ff9ef48dafc299771f0778e056760abe944c17
tomombutler <tom.butler.tb@gmail.com>
2015-04-08 08:56:05 +0100

    diff

--- js/scroll.js
+++ js/scroll.js
@@ -40,65 +40,51 @@
 
 // Nav appear
 
-$(".menu").on("click", function ( ev ) {
+$("nav").on("click", function ( ev ) {
 
-	$(".nav-expand").fadeIn(300);
+	$(".nav-overlay").fadeIn(300);
+    $("nav").fadeOut(300);
+    $("header h1").css("color", "#fefefe");
 });
 
 // Nav close
 
-$(".close-section").on("click", function(){
+$(".close").on("click", function(){
 
-	$(".nav-expand").fadeOut();
+	$(".nav-overlay").fadeOut(300);
+    $("nav").fadeIn(300);
+    $("header h1").css("color", "#181818");
 });
 
+//  sticky
 
-// parallax? 
+// Create a clone of the menu, right next to original.
+$('.project-header').addClass('original').clone().insertAfter('.project-header').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
 
-$(window).scroll(function() {
-  parallax();
-});
+scrollIntervalID = setInterval(stickIt, 10);
 
-function parallax() {
-  var scrolled = $(window).scrollTop();
-  $('.intro h2').css('top', -(scrolled * 0.3) + 'px');
+
+function stickIt() {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+
+  if ($(window).scrollTop() >= (orgElementTop) - 10) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left',leftOrgElement+'px').css('top',10).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
 }
-
-
-// img fade on load
-
-$(document).ready(function() {  
-    // fade in each image individually as it's downloaded  
-    $('img').load(function() {  
-        $(this).fadeIn('slow');  
-    });  
-});  
-
-// project fade on load
-
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.project').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top - 250 + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},400);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
 
 
 
